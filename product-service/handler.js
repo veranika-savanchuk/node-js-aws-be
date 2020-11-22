05dd2d2fb32a2179e962f1a77e85a079eb52b8d4
veranika-savanchuk <veranika.savanchuk@adevinta.com>
2020-11-22 11:49:13 +0300

    feat: add SQS and SNS

--- product-service/handler.js
+++ product-service/handler.js
@@ -2,9 +2,11 @@
 import { getProductById } from './handlers/getProductById';
 import { getProductsList } from './handlers/getProductsList';
 import { addProduct } from './handlers/addProduct';
+import { catalogBatchProcess } from './handlers/catalogBatchProcess';
 
 export {
     getProductById,
     getProductsList,
     addProduct,
+    catalogBatchProcess,
 }
